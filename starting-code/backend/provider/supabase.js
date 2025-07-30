2db26dfbaf480ea3ff4c4fa27f816a380822cb97
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-30 16:04:42 -0600

    Debug supabase authentication env variables

--- starting-code/backend/provider/supabase.js
+++ starting-code/backend/provider/supabase.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
 // Create two const variables `supabaseURL` and `supabaseSecret` with the value of the environment variables you created.
-const supabaseUrl = process.env.supabaseURL;
-const supabaseSecret = process.env.supabaseSecret;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseSecret = process.env.SUPABASE_SECRET;
 
 // Create a variable `supabase` and set the value to the execution of the `createClient` function passing in `supabaseUrl` and `supabaseSecret` variables as arguments.
 const supabase = createClient(supabaseUrl, supabaseSecret);
