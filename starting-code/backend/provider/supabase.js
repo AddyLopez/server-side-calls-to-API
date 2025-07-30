93b250df9205676cf684d446f0bdd7b7866b6a33
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-30 15:45:29 -0600

    Initialize supabase variable and create client

--- starting-code/backend/provider/supabase.js
+++ starting-code/backend/provider/supabase.js
@@ -7,6 +7,8 @@ const { createClient } = require("@supabase/supabase-js");
 // Create two const variables `supabaseURL` and `supabaseSecret` with the value of the environment variables you created.
 const supabaseURL = process.env.supabaseURL;
 const supabaseSecret = process.env.supabaseSecret;
+
 // Create a variable `supabase` and set the value to the execution of the `createClient` function passing in `supabaseUrl` and `supabaseSecret` variables as arguments.
+const supabase = createClient(supabaseURL, supabaseSecret);
 
 // Export the `supabase` variable.
